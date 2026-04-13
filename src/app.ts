f910cecd87039fc669e4cbe2563b8310340832f9
Harrypoggers25 <mfa.g4e@gmail.com>
2026-04-13 15:42:30 +0800

    Updated app

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,18 @@ import cookieParser from 'cookie-parser';
 
 // CONFIGS
 import env from "./configs/env.config";
+import { db } from "./configs/db.config";
+
+// APP
+import router from "./routers";
+
 App.listen({
     port: env.PORT,
     version: '1.0.0 build 1',
     cors: [env.ORIGIN_URL],
     beforeListen: async (app) => {
         app.use(cookieParser());
+        app.use('/', router);
     },
     callback: async (app, server) => {
     }
