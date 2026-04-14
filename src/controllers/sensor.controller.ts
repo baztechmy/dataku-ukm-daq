0bc439806565ac98232a2743e9b18055af5c9e48
Harrypoggers25 <mfa.g4e@gmail.com>
2026-04-14 12:29:56 +0800

    Added pair check for update sensor handler

--- src/controllers/sensor.controller.ts
+++ src/controllers/sensor.controller.ts
@@ -47,6 +47,8 @@ export const updateSensorHandler = Route.asyncHandler(async (req, res) => {
     const s_id = +req.params.s_id;
     const { s_index, s_addr, st_id } = req.body;
 
+    if ((s_index && !st_id) || (!s_index && st_id)) throw new Error(`Failed to update sensor [${s_id}]. s_index and st_id are required as a pair`);
+
     const oldSensor = await Sensor.find({ where: { s_index, st_id } });
     if (!oldSensor) throw new Error(`Failed to update sensor [${s_id}]. Unable to find old sensor with value {s_index: ${s_index}, st_id: ${st_id}}`);
     if (oldSensor.length > 0) {
