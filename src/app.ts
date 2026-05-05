8e8802ff5ce3535de75ba701be2040fdc40dcfff
Harrypoggers25 <mfa.g4e@gmail.com>
2026-05-05 13:54:08 +0800

    Build 6

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ function parseJson(str: string) {
 
 App.listen({
     port: env.PORT,
-    version: '1.0.0 build 5',
+    version: '1.0.0 build 6',
     cors: [env.ORIGIN_URL],
     beforeListen: async (app) => {
         app.use(cookieParser());
