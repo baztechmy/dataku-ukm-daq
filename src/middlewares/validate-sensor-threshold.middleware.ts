93fcae4547d1793e4e8fc8e45cffd84aa55e2dbf
Harrypoggers25 <mfa.g4e@gmail.com>
2026-05-07 16:27:11 +0800

    Code cleanup

--- src/middlewares/validate-sensor-threshold.middleware.ts
+++ src/middlewares/validate-sensor-threshold.middleware.ts
@@ -1,5 +1,21 @@
 import Route from "@harrypoggers25/route";
-import { verifySensorThreshold } from "../configs/db.config";
+
+type SensorThreshold = { threshold: number, symbol: '>=' | '>' | '<=' | '<' | '==' | '!=' };
+const verifySensorThreshold = (obj: SensorThreshold | Array<SensorThreshold> | null): { valid: boolean, message: string, result: null | string } => {
+    if (obj === null) return { valid: true, message: 'OK', result: null };
+    try {
+        const sensorThresholds: Array<SensorThreshold> = !Array.isArray(obj) ? [obj] : obj;
+        for (let i = 0; i < 2 && i < sensorThresholds.length; i++) {
+            const st = sensorThresholds[i];
+            if (Number.isNaN(+st.threshold)) throw new Error(`Threshold '${st.threshold}' is invalid. Threshold must be of type number`);
+            if (!['>=', '>', '<=', '<', '==', '!='].includes(st.symbol)) throw new Error(`Symbol '${st.symbol}' is invalid. Symbol must be either:  '>=', '>', '<=', '<', '==' or '!='`);
+        }
+
+        return { valid: true, message: 'OK', result: JSON.stringify(sensorThresholds) };
+    } catch (error: any) {
+        return { valid: false, message: error.message ?? error, result: null };
+    }
+}
 
 export const validateSensorThresholds = Route.asyncHandler(async (req, res, next) => {
     const { s_threshold_warning, s_threshold_critical } = req.body;
