88192d1663a5f1bc6cbac7e9e5778eb095a5c88c
Harrypoggers25 <mfa.g4e@gmail.com>
2026-05-07 17:33:22 +0800

    Build 9

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ import { mqttClient } from "./configs/mqtt.config";
 
 App.listen({
     port: env.PORT,
-    version: '1.0.0 build 8',
+    version: '1.0.0 build 9',
     cors: [env.ORIGIN_URL],
     beforeListen: async (app) => {
         app.use(cookieParser());
